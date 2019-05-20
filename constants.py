c7bb32fa61dc3eb3e6f63bb19bd8fdf6b23ecfa8
Mysteryquy <50675135+Mysteryquy@users.noreply.github.com>
2019-05-20 23:34:39 +0200

    Update 10
    Defiined Sprites for FOV

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import pygame
+import tcod as libtcodpy
 
 pygame.init()
 
@@ -25,7 +26,17 @@ COLOR_DEFAULT_BG = COLOR_GREY
 
 
 #SPRITES
+
 S_PLAYER = pygame.image.load("data/python.png")
-S_WALL = pygame.image.load("data/wall1.png")
-S_FLOOR = pygame.image.load("data/floor.jpg")
 S_ENEMY = pygame.image.load("data/enemy1.png")
+
+S_WALL = pygame.image.load("data/wall2.jpg")
+S_WALLEXPLORED = pygame.image.load("data/wallunseen.png")
+
+S_FLOOR = pygame.image.load("data/floor.jpg")
+S_FLOOREXPLORED = pygame.image.load("data/floorunseen.png")
+
+#FOV SETTINGS Er hat anstelle der Null "libcod.FOV_BASIC" da stehen
+FOV_ALGO = 0
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
