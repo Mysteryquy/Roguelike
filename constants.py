7d9b7e8a037ec73528d6c030f42868e951de385d
Mysteryquy <tobiasgum@gmail.com>
2019-07-25 21:08:00 +0200

    Merge branch 'master' of C:\Users\tobia\Desktop\EigenesRoguelike\Roguelike with conflicts.

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import pygame
-#import tcod as libtcodpy
+import tcod as libtcodpy
 
-
+pygame.init()
 
 GAME_WIDTH = 800
 GAME_HEIGHT = 600
@@ -21,28 +21,31 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_WHITE = (255, 255, 255)
 COLOR_GREY = (100, 100, 100)
 COLOR_RED = (255, 0, 0)
-COLOR_GREEN = (0, 255, 0)
-
-INVENTORY_TEXT_HEIGHT = 20
 
 
 #game colors
 COLOR_DEFAULT_BG = COLOR_GREY
 
 
+#SPRITES
 
+S_PLAYER = pygame.image.load("data/python.png")
+S_ENEMY = pygame.image.load("data/enemy1.png")
 
+S_WALL = pygame.image.load("data/wall2.jpg")
+S_WALLEXPLORED = pygame.image.load("data/wallunseen.png")
 
-
-
-
+S_FLOOR = pygame.image.load("data/floor.jpg")
+S_FLOOREXPLORED = pygame.image.load("data/floorunseen.png")
 
 #FOV SETTINGS Er hat anstelle der Null "libcod.FOV_BASIC" da stehen
 FOV_ALGO = 0
 FOV_LIGHT_WALLS = True
 TORCH_RADIUS = 10
 
-
+#FONT SETTINGS
+FONT_DEBUG_MESSAGE = pygame.font.Font("data/joystix.ttf", 20)
+FONT_MESSAGE_TEXT = pygame.font.Font("data/joystix.ttf", 12)
 
 #MESSAGE DEFAULTS
 NUM_MESSAGES = 4
